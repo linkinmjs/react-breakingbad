2229c302ddc388fb9f05049ed0eb2df464946892
Mauri <linkinmjs@gmail.com>
2020-05-31 20:26:58 -0300

    Obteniendo la frase desde la API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
   background-size: 300px;
   font-family: Arial, Helvetica, sans-serif;
   color: #fff;
@@ -10,27 +15,25 @@ const Boton = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
-`
+`;
 
 const Contenedor = styled.div`
-  display:flex;
+  display: flex;
   align-items: center;
   padding-top: 5rem;
   flex-direction: column;
-`
+`;
 
-const  consultarAPI = () => {
-  console.log('consultando');
-}
+const consultarAPI = async() => {
+  const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  const frase = await api.json();
+  console.log(frase[0]);
+};
 
 function App() {
   return (
     <Contenedor>
-      <Boton
-        onClick={consultarAPI}
-      >
-        Obtener Frase
-      </Boton>
+      <Boton onClick={consultarAPI}>Obtener Frase</Boton>
     </Contenedor>
   );
 }
