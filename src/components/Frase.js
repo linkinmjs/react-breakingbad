0d8834e5d79693046cd22f77a19d2301239d2556
Mauri <linkinmjs@gmail.com>
2020-05-31 21:21:51 -0300

    añadiendo configuración de protocolo http

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -4,43 +4,39 @@ import styled from '@emotion/styled'
 const ContenedorFrase = styled.div`
     padding: 3rem;
     border-radius: .5rem;
-    background-color: #FFF;
-    max-width: 800px;
+    background-color: #fff;
+    max-width:800px;
     
-    @media (min-width: 992px){
-        margin-top:10rem;
+    @media (min-width: 992px) {
+        margin-top: 10rem;
     }
-
-    h1{
+    h1 {
         font-family: Arial, Helvetica, sans-serif;
         text-align: center;
         position: relative;
         padding-left: 4rem;
-
-        &::before{
+        &::before {
             content: open-quote;
             font-size: 10rem;
             color: black;
-            position: absolute; 
+            position: absolute;
             left: -1rem;
             top: -2rem;
         }
     }
-
-    p{
-        font-family:Verdana, Geneva, Tahoma, sans-serif;
-        font-size: 1.6rem;
-        font-weight: bold;
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight:bold;
         text-align: right;
         color: #666;
         margin-top: 2rem;
-
     }
 `;
 
 const Frase = ({frase}) => {
 
-    
+
 
     return ( 
         <ContenedorFrase>
